2e753ab8944254e80133aa1fc2de6c98a3172541
matrix <64998320+shnider0@users.noreply.github.com>
2021-08-11 19:40:58 +0300

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -4,20 +4,14 @@
 import os
 
 class Config(object):
-    API_HASH = "fbdeb2f589e08ddbb6ed5c8c0a484d46"
-    API_ID = int("4967192")
-
-    TG_BOT_TOKEN = "1907690033:AAGPyT5c815vWeapzq0hw4DTnDDvGv1giWQ"
-
-    MONGO_URL = "mongodb+srv://shnider:shnider1@cluster0.z64ay.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-    owner_id = int(1827146773)
-    # TG_BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-    #
-    # APP_ID = int(os.environ.get("API_ID", 12345))
-    #
-    # API_HASH = os.environ.get("API_HASH", "")
-    # MONGO_URL =  os.environ.get("MONGO_URL",  "")
-    # owner_id =int(1827146773)
+    
+    TG_BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    
+    APP_ID = int(os.environ.get("API_ID", 12345))
+    
+    API_HASH = os.environ.get("API_HASH", "")
+    MONGO_URL =  os.environ.get("MONGO_URL",  "")
+    owner_id =int(1827146773)
 
 
 
