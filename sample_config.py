4a752f03ace2fafdb3b7debd66a35522caad3090
matrix <64998320+shnider0@users.noreply.github.com>
2021-08-11 19:45:34 +0300

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -7,7 +7,7 @@ class Config(object):
     
     TG_BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
     
-    APP_ID = int(os.environ.get("API_ID", 12345))
+    API_ID = int(os.environ.get("API_ID", 12345))
     
     API_HASH = os.environ.get("API_HASH", "")
     MONGO_URL =  os.environ.get("MONGO_URL",  "")
