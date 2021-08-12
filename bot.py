f7f6c4746fc6fccfb54d1a0d817110a680908b37
matrix <64998320+shnider0@users.noreply.github.com>
2021-08-12 17:56:07 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,7 +16,7 @@ if __name__ == "__main__" :
         api_id=Config.API_ID,
         api_hash=Config.API_HASH,
         plugins=plugins,
-        workers=400,
+        workers=300,
     )
     app.run()
 
